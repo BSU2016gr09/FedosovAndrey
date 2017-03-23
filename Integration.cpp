0440b02c8c685a06b8330a674220e0c6016a3430
AndreyFedosov1 <kadac28@gmail.com>
2017-03-23 23:00:47 +0300

    classwork UPD

---
+++ Integration.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+//посчитать интеграл некоторых функций
+using namespace std;
+
+typedef float(*funcs)(float);
+typedef funcs pf;
+
+float mysin(float x) { return sin(x); };
+float sqrtX(float x) { return x*x; }
+float mycos(float x) { return cos(x); }
+float inverse(float x) { return 1 / (x*1.0); }
+
+float integrate(float, float, pf, float);
+void help();
+
+int main() {
+	funcs menu[] = { mysin,sqrtX,mycos,inverse};
+	
+	float a, b;
+	int number;
+	help();cin >> number;
+	
+	while (number){
+		cout << "Type limits of the integration\n";cin >> a >> b;
+		cout << "Type accuracy\n";float eps;cin >> eps;
+		cout << integrate(a, b, menu[number], eps) << endl;
+	
+		cout << "Type a number again\n";
+		cin >> number;
+	} 
+
+	system("pause");
+}
+
+void help() {
+	cout << "Type 0 for exit\n";
+	cout << "Type 1 for integration sin(x)\n";
+	cout << "Type 2 for integration cos(x)\n";
+	cout << "Type 3 for integration 1/x\n";
+	cout << "Type 4 for integration x^2\n";
+}
+float integrate(float a, float b, pf func, float eps) {
+	int n = 50;
+	float res = 0, prevres = 0;
+	do
+	{
+		prevres = res;
+		res = 0;
+		float width = abs((b - a) / n);
+		for (float i = a; i <= b-width; i += width) {
+
+			res += func(i)*width;
+		}
+		n++;
+		
+	} while (abs(prevres-res) > eps);
+	cout << "Final partioning = " << n << endl<<"Result = ";
+	return res;
+}
