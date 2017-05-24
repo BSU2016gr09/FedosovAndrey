3ece8685981ef8634271eef40368e657cc0e9116
AndreyFedosov1 <kadac28@gmail.com>
2017-05-24 16:41:41 +0300

    completed

--- book.cpp
+++ book.cpp
@@ -1,15 +1,26 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
-//Улучшу ввод и хранение массива книг когда подключу файлы,пока просто показываю,что все функции готовы и работают
 #include <iostream>
 #include <vector>
 #include<algorithm>
+#include<fstream>
 #include<ctime>
+/*
+Правило записи книги во входной файл:
 
+Имя автора
+Фамилия автора
+Название книги
+Год издания Количество экземпляров Стоимость одного
+
+*/
 using namespace std;
+ifstream in("input.txt",ios_base::in);
+ofstream out("output.txt", ios_base::out | ios_base::trunc);
+void checkFiles();
 
 class Book {
-	friend ostream& operator<<(ostream&, const Book&);
-	friend istream& operator >> (istream&, Book&);
+	friend ofstream& operator<<(ofstream&, const Book&);
+	friend ifstream& operator >> (ifstream&, Book&);
 public:
 	friend void giveMemory(char*&str, int size);	
 	Book(){//конструктор по умолчанию
@@ -27,7 +38,8 @@ public:
 	}
 	Book(const Book &obj) {//конструктор копии
 		
-		this->cipherTopic = this->cipherBook = this->year = this->amount = this->cost = 0;
+		this->cipherTopic = obj.cipherTopic;this->cipherBook = obj.cipherBook;this->year = obj.year;
+		this->amount = obj.amount; this->cost = obj.cost;
 		this->name = this->surname = this->title = nullptr;
 		giveMemory(this->name, 100);giveMemory(this->surname, 100);giveMemory(this->title, 100);
 		strcpy(this->name, obj.name);strcpy(this->surname, obj.surname);strcpy(this->title, obj.title);
@@ -43,7 +55,7 @@ public:
 		return *this;
 	}
 	~Book(){
-		//cout << "Destructor is working\n";system("pause");
+		//out << "Destructor is working\n";system("pause");
 		delete[]name;
 		delete[]surname;
 		delete[]title;
@@ -51,7 +63,7 @@ public:
 	}
 
 	void setTopic(){
-		cipherTopic = rand() % 3 + 1;//1-novel;2-adventure;3-science;
+		cipherTopic = rand() % 3 + 1;
 	}
 	/*void setYear(int year){
 		this->year = year;
@@ -75,22 +87,27 @@ public:
 		fullName = nullptr;
 
 	}
-	char* getAuthorsSurname() {
+	char * getName()const {
+		return name;
+	}
+	char* getSurname()const {
 		return surname;
+
 	}
-	char* getTitle() {
+	char* getTitle()const {
 		return title;
+
 	}
-	int getCost() {
+	int getCost() const{
 		return cost;
 	}
-	int getTopic() {
+	int getTopic() const{
 		return cipherTopic;
 	}
-	int getYear() {
+	int getYear() const{
 		return year;
 	}
-	int getAmount() {
+	int getAmount()const {
 		return amount;
 	}
 private:
@@ -104,98 +121,47 @@ typedef func pf;
 void printLibrary(vector<Book>&,pf);
 void printBooksUpToTopicAndYear(vector<Book>&, int,int);
 void printAuthorsBook(vector<Book>&,char*);
+void deleteByAuthor(vector<Book>&, char*);
+void deleteByYear(vector<Book>&, int );
 bool cmpYear(Book&, Book&);
 bool cmpSurname(Book&, Book&);
 bool cmpTitle(Book&, Book&);
-void deleteByAuthor(vector<Book>&, char*);
-void deleteByYear(vector<Book>&, int );
-
+void workWithLibrary(vector<Book>);
 
 
 void main() {
 	srand(time(0));
-	vector <Book> library;library.reserve(6);
-	Book book1("Fedor", "Dostoevskiy", "Idiot", 2003, 52, 140), book2("Fedor", "Dostoevskiy", "Prestuplenie i nakazanie", 2017, 5, 1000), book3("Edmund", "Zverovich", "Analiz", 2007, 17, 523), book4("Lev", "Talstoy", "Voina i mir", 1980, 1, 666), book5, book6;
-	cin >> book5;
-	cin.ignore();
-	//cin >> book6;
-	library.push_back(book1);
-	library.push_back(book2);
-	library.push_back(book3);
-	library.push_back(book4);
-	library.push_back(book5);
-	//library.push_back(book6);
-
+	checkFiles();
+	vector <Book> library;library.reserve(10);
+	Book book;
+	for(int i=0;i<5;i++){
+		in >> book;
+	    in.ignore();
+		library.push_back(book);
+	}
 	for (unsigned i = 0;i < library.size();i++) //не понял что такое шифр темы и шифр книги,поэтому не запаривался пока что
 		library[i].setTopic();
 
-	cout << "Current library:\n";
-	printLibrary(library, cmpYear);
-	cout << "Print up to topic 1(topic of each book is random[1-3]) after a 2001 year:\n";
-	printBooksUpToTopicAndYear(library, 1, 2001);
-
-	cout << "Printing books of Fedor Dostoevskiy:\n";
-	printAuthorsBook(library, "Fedor Dostoevskiy");
-
-	int mode;
-	while (1) {
-		system("pause");
-		system("cls");
-		cout << "How do you want to print the library?Sort by title(1), by author's surname(2),by year of publication(3)\n";
-		cin >> mode;
-		cout << "CURRENT LIBRARY:\n\n";
-		switch (mode)
-		{
-		case 1: {printLibrary(library, cmpTitle);break;}
-		case 2: {printLibrary(library, cmpSurname);break;}
-		case 3: {printLibrary(library, cmpYear);break;}
-		default: {cout << "Error in case\n";system("pause");}
-		}
-		int quit = 1;
-		cout << "if you want to exit type 0\n";cin >> quit;
-		if (!quit) break;
-	}
-	int year;
-	while (1) {
-		system("pause");
-		system("cls");
-		cout << "Remove Dostoevsky books(1),delete by year(2)\n";
-		cin >> mode;
-		switch (mode)
-		{
-		case 1: {deleteByAuthor(library, "Fedor Dostoevskiy");cout << "CURRENT LIBRARY:\n\n";printLibrary(library, cmpYear);break;}
-		case 2: {cin >> year;deleteByYear(library, year);cout << "CURRENT LIBRARY:\n\n";printLibrary(library, cmpYear);break;}
-		default: {cout << "Error in case\n"; system("pause");}
-		}
-		int quit = 1;
-		cout << "if you want to exit type 0\n";cin >> quit;
-		if (!quit) break;
-	}
 	
+	workWithLibrary(library);
 	system("pause");
 }
 
 
 
-ostream& operator<<(ostream& output, const Book& book)
+ofstream& operator<<(ofstream& output,const Book& book)
 {
-	output << book.name << " " << book.surname << endl << book.title << "," <<"year "<<book.year << endl <<book.amount<<" books, "<< book.cost << "$ for each" << endl<<endl;
+	output << book.getName() << " " << book.getSurname() << endl << book.getTitle() << "," << "year " << book.getYear()
+		<< endl << book.getAmount() << " books, " << book.getCost() << "$ for each" << endl<<endl;
 	return output;
 }
-istream& operator >> (istream& input, Book& book)
+ifstream& operator >> (ifstream& input, Book& book)
 {
-	cout << "Type description of a book by the following way:\nName\nSurname\nTitle\nYear Amount Cost\n\n";
-
-	
 	input.getline(book.name,100,'\n');
-	
-	input.getline(book.surname, 100,'\n');
-
+	input.getline(book.surname, 100, '\n');
 	input.getline(book.title, 100,'\n');
 	
 	input >> book.year >>book.amount>> book.cost;
-	cout << endl;
-	
 	return input;
 }
 void giveMemory(char*&str,int size)
@@ -205,34 +171,34 @@ void giveMemory(char*&str,int size)
 	}
 	catch (...)
 	{
-		cout << "Allocation failed\n";
+		out << "Allocation failed\n";
 		system("pause");
 	}
 }
 bool cmpYear(Book&b1, Book&b2) {return (b1.getYear() < b2.getYear());}
-bool cmpSurname(Book&b1, Book&b2) { if (strcmp(b1.getAuthorsSurname(), b2.getAuthorsSurname()) < 0) return true;else return false; }
+bool cmpSurname(Book&b1, Book&b2) { if (strcmp(b1.getSurname(), b2.getSurname()) < 0) return true;else return false; }
 bool cmpTitle(Book&b1, Book&b2) { if (strcmp(b1.getTitle(), b2.getTitle()) < 0) return true;else return false; }
 
 void printLibrary(vector<Book>&library,pf func) { //D
+	out << "LIBRARY SORTED:\n";
 	sort(library.begin(), library.end(),func);
 	int libraryCost=0, amount=library.size();
 	for (auto it = library.begin(); it != library.end(); ++it) {
-		cout << (*it);
+		out << (*it);
 		libraryCost += (*it).getCost();
 	}
-	cout << "There are " << amount << " books in library.The total cost of books (one copy) " << libraryCost << "$\n\n";
+	out << "There are " << amount << " books in library.The total cost of books (one copy) " << libraryCost << "$\n\n";
 }
 void printBooksUpToTopicAndYear(vector<Book>&library, int topic,int year) {// B
+	out << "LIBRARY(GIVEN TOPIC AFTER GIVEN YEAR\n";
 	for (auto it = library.begin(); it != library.end(); ++it)
-		if((*it).getTopic()==topic&&(*it).getYear()>year) cout << (*it);
-
+		if((*it).getTopic()==topic&&(*it).getYear()>year) out << (*it);
 }
 void printAuthorsBook(vector<Book>&library,char*author) { // C
-
+	out << "Books of " << author << endl;
 	sort(library.begin(), library.end(), cmpYear);
 	for (auto it = library.begin(); it != library.end(); ++it)
-		if (!strcmp((*it).getAuthor(), author))  cout << (*it);
-		
+		if (!strcmp((*it).getAuthor(), author))  out << (*it);	
 }
 void deleteByAuthor(vector<Book>&library, char*author) {
 	for (auto it = library.begin(); it != library.end();) 
@@ -243,4 +209,78 @@ void deleteByYear(vector<Book>&library, int year) {
 	for (auto it = library.begin(); it != library.end();)
 		if ((*it).getYear()==year) it = library.erase(it);
 		else ++it;
+}
+void checkFiles()
+{
+	if (!in.is_open()) {
+		cout << "error input.txt!\n";
+		system("pause");
+		exit(0);
+	}
+	if (!out.is_open())
+	{
+		cout << "error output.txt !\n";
+		system("pause");
+		exit(0);
+	}
+}
+
+void workWithLibrary(vector<Book>library) {
+	int stop, type, rem, print,year;
+	char*author;
+	giveMemory(author, 200);
+	while (1) {
+		cout << "Select type of work:\n";
+		cout << "Print(1)\nRemoving(2)\n";
+		cin >> type;
+		switch (type)
+		{
+		case 1: {
+			cout << "Select type of printing:\n"; //не добавлял сюда печать по теме после года издания,
+			cout << "Print books of the given author(1)\nPrint books sorting by year(2), author's surname(3), book title(4)\n";
+			cin >> print;
+			switch (print)
+			{
+			case 1: {
+				cout << "Enter fullname of author,whose books you want to print:\n";
+				cin.ignore();
+				cin.getline(author, 200);
+				printAuthorsBook(library, author);
+				break;
+			}
+			case 2: { printLibrary(library, cmpYear); break;}
+			case 3: { printLibrary(library, cmpSurname); break; }
+			case 4: { printLibrary(library, cmpTitle); break;}
+			default: {cout << "Error in switch printing\n";system("pause");exit(0);break;}
+			}
+			break;
+		}
+		case 2: {
+			cout << "Select type of removing:\n";
+			cout << "By author(1)\nBy year(2)\n";
+			cin >> rem;
+			switch (rem)
+			{
+			case 1: {
+				cout << "Enter fullname of author,whose books you want to delete:\n";
+				cin.ignore();
+				cin.getline(author, 200);
+				deleteByAuthor(library, author);
+				break;
+			}
+			case 2: {
+				cout << "Enter the year of publication of the books you want to delete:\n";
+				cin >> year;
+				deleteByYear(library, year);
+				break;
+			}
+			default: {cout << "Error in switch removing\n";system("pause");exit(0);break;}
+			}
+		}
+		default: { break;}
+		}
+		cout << "If you want to stop your work enter now \'0\',else enter any number\n";cin >> stop;
+		if (!stop) return;
+	}
+	delete[] author;
 }
